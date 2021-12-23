6f627902afe5737df13d7d78b7974de68ba771e3
mollybeach <mollyjbeach@gmail.com>
2021-12-23 08:51:32 -1000

    update

--- tips.js
+++ tips.js
@@ -2,24 +2,26 @@
 
 TESTING STEPS
 1. Make sure in hardhat .config.js file you have the following:
-    require("./tasks/deploy_name_of_your_task.js");
+        require("./tasks/deploy_name_of_your_task.js");
 1. Open terminal and navigate to the stacked toads directory    
         npx hardhat compile
 
 2. Open another terminal and navigate to the stacked toads directory 
         FORK_MODE=true npx hardhat node 
+        
 //mint before successful deployment
 3. In original terminal in the stacked toads directory, run the following command to test/mint the contract:
         yarn run hardhat deploy-name-of-contract --network localhost
-        yarn run hardhat deploy-fud-farm-genesis --network localhost
 
-//You'll know it's working if you see the following in /stacked_toads/data/deployments.json:
+//You'll know it's working if you see the following in //data/deployments.json:
         "NameOfContract": "0xf2A60c06479779823734F8877D160836a23C9525"
 4. After successful deployment:
-    comment the following line in deploy_name_of_your_task.js file:
-    //const instance = await factory.deploy(NAME, SYMBOL, BASE_URI);
-    and uncomment the following line in the stacked toads directory:
-    const instance = await ethers.getContractAt('NameOfContract',deployments.NameOfContract);
+        Comment the following line in deploy_name_of_your_task.js file:
+        //const instance = await factory.deploy(NAME, SYMBOL, BASE_URI);
+
+        Uncomment the following line in deploy_name_of_your_task.js file:
+        const instance = await ethers.getContractAt('NameOfContract',deployments.NameOfContract);
+
 5. kill all terminal windows and repeat steps 1-4 with the instance redefined 
 
 
@@ -43,7 +45,7 @@ TESTING STEPS
 
 
 
-
+yarn run hardhat deploy-fud-farm-genesis --network localhost
 npx hardhat compile
 npx hardhat test
 npx hardhat deploy-default-20 --network rinkeby
